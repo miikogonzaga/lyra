b70e01cc43a3971c0c9c3c0e8066220ba5fbd8c4
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2017-02-26 11:38:16 -0800

    Add conditional to animate elements on taller screens #10

--- public/js/main.js
+++ public/js/main.js
@@ -9,15 +9,20 @@ const animatePoints = (points) => {
     points[index].style.WebkitTransform = "scaleX(1) translateY(0)"
   }
 
+  // Loops through each
   for (let i = 0; i < points.length; i++) { revealPoint(i) }
 
 }
 
 window.onload = () => {
 
-  const sellingPoints = document.getElementsByClassName('selling-points')[0]
-  const scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 100
+  // Animates the selling points for taller screens
+  if (window.innerHeight > 950) animatePoints(points)
 
+  const sellingPoints = document.getElementsByClassName('selling-points')[0]
+  const scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200
+
+  // Animates the selling points when the user scrolls 200 pixels into view
   window.addEventListener('scroll', () => {
     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
       animatePoints(points)
